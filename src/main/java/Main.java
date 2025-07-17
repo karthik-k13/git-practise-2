c2e828fbfa9bd07c4c8a9d9a98a13290412cef53
karthik-k13 <karthik.krishnareddy.mahadi@gmail.com>
2025-07-17 13:58:53 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    static String  branchName="master-sub1";
+    static String  branchName="master-sub-001";
 
     public static void main(String[] args) {
         System.out.println(branchName);
